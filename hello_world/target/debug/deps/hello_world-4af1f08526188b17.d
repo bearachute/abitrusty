82ff4ead6c39c9c1ee8667519e93eca6943fa8da
bearachute <sjourand@gmail.com>
2018-06-22 23:36:00 -0400

    pun on words

---
+++ hello_world/target/debug/deps/hello_world-4af1f08526188b17.d
@@ -0,0 +1,5 @@
+/Users/skt/Desktop/abitrusty/hello_world/target/debug/deps/hello_world-4af1f08526188b17: src/main.rs
+
+/Users/skt/Desktop/abitrusty/hello_world/target/debug/deps/hello_world-4af1f08526188b17.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/skt/Desktop/abitrusty/hello_world/target/debug/hello_world.dSYM: /Users/skt/Desktop/abitrusty/hello_world/src/main.rs
